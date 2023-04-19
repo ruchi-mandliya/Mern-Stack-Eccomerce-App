bd7fa9a2e7f761592302c7da0426137d7fb96a17
Ruchi Mandliya <rmandliya18@gmail.com>
2023-04-19 13:59:14 +0530

    compare password

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -1,10 +1,15 @@
 import bcrypt from "bcrypt";
 
-export const hashedPassword = async (password) => {
+export const hashPassword = async (password) => {
   try {
     const saltRounds = 10;
     const hashedpassword = await bcrypt.hash(password, saltRounds);
+    return hashedPassword;
   } catch (error) {
     console.log(error);
   }
+};
+
+export const comparePassword = async (password, hashedPassword) => {
+  return bcrypt.compare(password, hashedPassword);
 };
