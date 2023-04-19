22612d7868815a4a66d81f6b1e3bcd597ab8c359
Ruchi Mandliya <rmandliya18@gmail.com>
2023-04-19 14:57:45 +0530

    create route for register

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,9 @@
 // import { comparePassword, hashPassword } from "./../helpers/authHelper.js";
 // import JWT from "jsonwebtoken";
 
+import { hashPassword } from "./../helpers/authHelper.js";
+import userModel from "../models/userModel.js";
+
 // export const registerController = async (req, res) => {
 //   try {
 //     const { name, email, password, phone, address } = req.body;
@@ -118,6 +121,58 @@
 //   }
 // };
 
-export const registerController = () => {};
+export const registerController = async (req, res) => {
+  try {
+    const { name, email, password, phone, address } = req.body;
+
+    if (!name) {
+      return res.send({ error: "Name is Required" });
+    }
+    if (!email) {
+      return res.send({ error: "Email is Required" });
+    }
+    if (!password) {
+      return res.send({ error: "Password is Required" });
+    }
+    if (!phone) {
+      return res.send({ error: "Phone no is Required" });
+    }
+    if (!address) {
+      return res.send({ error: "Address is Required" });
+    }
+
+    const exisitingUser = await userModel.findOne({ email });
+
+    if (exisitingUser) {
+      return res.status(200).send({
+        success: true,
+        message: "Already Register please login",
+      });
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const user = await new userModel({
+      name,
+      email,
+      phone,
+      address,
+      password: hashedPassword,
+    }).save();
+
+    res.status(201).send({
+      success: true,
+      message: "User Register Successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Errro in Registeration",
+      error,
+    });
+  }
+};
 
 export const loginController = () => {};
--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt";
 export const hashPassword = async (password) => {
   try {
     const saltRounds = 10;
-    const hashedpassword = await bcrypt.hash(password, saltRounds);
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
     return hashedPassword;
   } catch (error) {
     console.log(error);
