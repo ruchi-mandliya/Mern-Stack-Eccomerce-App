90586504ed4fdf2bbe9c8769f3f2beeda24f3e55
Ruchi Mandliya <rmandliya18@gmail.com>
2023-04-20 16:31:30 +0530

    design contact page

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Footer from "./Footer.jsx";
 import Header from "./Header.jsx";
+import { Helmet } from "react-helmet";
 
 const Layout = ({ children }) => {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My title</title>
+      </Helmet>
+
       <Header />
       <main style={{ minHeight: "70vh" }}>{children}</main>
       <Footer />
