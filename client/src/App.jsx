9958ddff34baed144e8cffac92761d54369311ba
Ruchi Mandliya <rmandliya18@gmail.com>
2023-04-20 14:05:41 +0530

    design footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
-import React from "react";
-import Layout from "./components/Layout/Layout.jsx";
-import "./App.css";
-
-const App = () => {
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Policy from "./pages/Policy";
+import Pagenotfound from "./pages/Pagenotfound";
+function App() {
   return (
     <>
-      <Layout>
-        <h1>Eccomerce App</h1>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<Pagenotfound />} />
+      </Routes>
     </>
   );
-};
+}
 
 export default App;
--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Footer from "./Footer.jsx";
+import Header from "./Header.jsx";
 
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      <main style={{ minHeight: "70vh" }}>{children}</main>
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
