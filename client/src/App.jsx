7bd586f399fb9ae7e7e54d8de8633a8e5d3577a6
Ruchi Mandliya <rmandliya18@gmail.com>
2023-04-20 11:00:09 +0530

    frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "./components/Layout/Layout.jsx";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Layout>
+        <h1>Eccomerce App</h1>
+      </Layout>
+    </>
+  );
+};
+
+export default App;
