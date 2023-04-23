4cd6737b8e81bfdfe72637d0541ed25e36faf3f7
Ruchi Mandliya <rmandliya18@gmail.com>
2023-04-23 11:08:54 +0530

    design login page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Contact from "./pages/Contact";
 import Policy from "./pages/Policy";
 import Pagenotfound from "./pages/Pagenotfound";
 import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
 function App() {
   return (
     <>
@@ -13,6 +14,8 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+
         <Route path="/policy" element={<Policy />} />
         <Route path="*" element={<Pagenotfound />} />
       </Routes>
