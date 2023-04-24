65894491b75f139a3d9b5f79f335738667e79d49
Ruchi Mandliya <rmandliya18@gmail.com>
2023-04-24 09:49:22 +0530

    createContext for managing globalState

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,15 @@ import "./index.css";
 import App from "./App";
 
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/auth";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    {/* <React.StrictMode> */}
-    {/* <App /> </React.StrictMode> */}
-    <App />
-  </BrowserRouter>
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
